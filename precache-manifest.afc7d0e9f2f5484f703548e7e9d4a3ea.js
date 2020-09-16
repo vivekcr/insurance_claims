165267ff55ab37ad4a48cd0076433c4f704d66a7
Vivek Chickkaiyur Ramachandra <Vivek.ChickkaiyurRamachandra@macquarie.com>
2020-09-16 14:15:15 +1000

    Updates

---
+++ precache-manifest.afc7d0e9f2f5484f703548e7e9d4a3ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5282f968a97c7d1da67a5f4846c21bf4",
+    "url": "/insurance_claims/index.html"
+  },
+  {
+    "revision": "8dfbb34653d44a042b22",
+    "url": "/insurance_claims/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "596e45deeb18790b833b",
+    "url": "/insurance_claims/static/css/main.11452a80.chunk.css"
+  },
+  {
+    "revision": "8dfbb34653d44a042b22",
+    "url": "/insurance_claims/static/js/2.4837c030.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/insurance_claims/static/js/2.4837c030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596e45deeb18790b833b",
+    "url": "/insurance_claims/static/js/main.074e80fe.chunk.js"
+  },
+  {
+    "revision": "df80ccffa33f9b9f1937",
+    "url": "/insurance_claims/static/js/runtime-main.b9b32193.js"
+  }
+]);
